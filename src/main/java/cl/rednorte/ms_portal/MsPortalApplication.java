ebeebeaff5049f38c4c975c5c08f5d7ba5047503
Oscar Garrido <osca.garrido@duocuc.cl>
2026-04-28 18:02:50 -0400

    Configuración inicial

---
+++ src/main/java/cl/rednorte/ms_portal/MsPortalApplication.java
@@ -0,0 +1,13 @@
+package cl.rednorte.ms_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPortalApplication.class, args);
+	}
+
+}
